d5a8fda039ca032f90ec6c582062eb5229effe57
wcff188-beep <wcff188@gmail.com>
2026-07-18 08:16:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const CONFIG = {
     // Ganti dengan URL Web App dari Google Apps Script setelah deploy
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzW3UrNpQs9-mWMBe_Fwf8_82Z_oIkzHgQH6SrE7YyNBSR3hYMeKn2ZUx9yg6b2-DpNDQ/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwyG7RnSDWkl-AX7Fl6PWjKDlfSOSQJR-xQBnIYAuH_ZmUOn2Lpk8gBNWMkuFSSEBsGkA/exec"
 };
